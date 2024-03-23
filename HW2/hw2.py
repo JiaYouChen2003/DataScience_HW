798d497bf2933d9a78249287df93ff4eb44ec334
JiaYouChen2003 <justin041510@gmail.com>
2024-03-23 16:58:10 +0800

    :bookmark: By changing model to palm and tune the prompt, finish the prompt testing, get 0.776, better than the strong baseline
    :see_no_evil: Ignore record file that record prompt test result
    :see_no_evil: Ignore palm_key file that have the palm key

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import promptbench as pb
+import time
 import tqdm
 
 
@@ -7,19 +8,45 @@ file_path = './mmlu_sample.csv'
 data = pd.read_csv(file_path)
 data_list = data.to_dict(orient='records')
 
-model = pb.LLMModel(model='google/flan-t5-large', max_new_tokens=10, temperature=0.0001)
+promptList = pb.Prompt(["The following are multiple choice questions (with answers) about {task}. You are an expert in {task}. You will be given a question in {task}.\n\
+                        '''{input}'''\n\
+                        Which one of the four choices is correct about the question, (A), (B), (C) or (D)?:\n\
+                        Choices:\n\
+                        (A) {A}\n\
+                        (B) {B}\n\
+                        (C) {C}\n\
+                        (D) {D}\n\'''",
+                        "The following are multiple choice questions (with answers) about {task}. You are an expert in {task}. You will be given a question in {task}.\n\
+                        '''{input}'''\n\
+                        What is the best choice for the question, (A), (B), (C) or (D)?:\n\
+                        Choices:\n\
+                        (A) {A}\n\
+                        (B) {B}\n\
+                        (C) {C}\n\
+                        (D) {D}\n\'''",
+                        "The following are multiple choice questions (with answers) about {task}. You are an expert in {task}. You will be given a question in {task}.\n\
+                        '''{input}'''\n\
+                        Which one of the four choices completes the question correctly, (A), (B), (C) or (D)?:\n\
+                        Choices:\n\
+                        (A) {A}\n\
+                        (B) {B}\n\
+                        (C) {C}\n\
+                        (D) {D}\n\'''",
+                        "The following are multiple choice questions (with answers) about {task}. You are an expert in {task}. You will be given a question in {task}.\n\
+                        '''{input}'''\n\
+                        Choices:\n\
+                        (A) {A}\n\
+                        (B) {B}\n\
+                        (C) {C}\n\
+                        (D) {D}\n\'''"
+                        ])
 
-prompts = pb.Prompt(["Answer the following multiple choices question related to {task}, think step by step, but only provide answer with A, B, C, D: {input}\n\
-                     A: {A}\n\
-                     B: {B}\n\
-                     C: {C}\n\
-                     D: {D}"
-                     ])
 
-
-def extractSubstringForResponse(string, start_index=4):
-    print(string)
-    string = string.replace(" ", "")
+def extractSubstringForResponse(string, start_index=-1):
+    if string is None:
+        string = 'DCBA'
+    string = string.replace(" ", "").replace("(", "").replace(")", "")
+    print('\n' + string)
     return string[start_index + 1:start_index + 2]
 
 
@@ -33,21 +60,56 @@ def projFunc(pred):
     return mapping.get(pred, -1)
 
 
-for prompt in prompts:
+if __name__ == '__main__':
+    with open('./palm_key.txt', 'r') as file:
+        palm_key = file.read()
+        model = pb.LLMModel(model='palm', max_new_tokens=10, temperature=0.0001, palm_key=palm_key)
+    count = 0
+    taskType_list = ['high_school_european_history',
+                    'high_school_us_history',
+                    'high_school_world_history',
+                    'high_school_microeconomics',
+                    'high_school_biology',
+                    'high_school_government_and_politics',
+                    'high_school_geography',
+                    'high_school_psychology',
+                    'high_school_computer_science',
+                    'high_school_macroeconomics']
     preds = []
     labels = []
+    
     for data in tqdm.tqdm(data_list):
-        input_text = pb.InputProcess.basic_format(prompt, data)
+        input = data['input']
         label = data['target']
+        taskType = data['task']
+        data['task'] = data['task'].replace("_", " ")
         
-        raw_pred = extractSubstringForResponse(model(input_text))
+        if str(input).replace(" ", "").endswith('?'):
+            prompt = promptList[3]
+        elif taskType in ['high_school_european_history',
+                        'high_school_us_history',
+                        'high_school_world_history']:
+            prompt = promptList[0]
+        else:
+            prompt = promptList[2]
+        
+        input_text = pb.InputProcess.basic_format(prompt, data)
+        
+        try:
+            output = model(input_text)
+        except:
+            output = None
+            time.sleep(10)
+        
+        raw_pred = extractSubstringForResponse(output)
         pred = projFunc(raw_pred)
         label = projFunc(label)
         print(pred, label)
         
         preds.append(pred)
         labels.append(label)
-    
-    # evaluate
-    score = pb.Eval.compute_cls_accuracy(preds, labels)
-    print(f"{score:.3f}, {prompt}")
+        
+    with open('./record.txt', 'a') as file:
+        score = pb.Eval.compute_cls_accuracy(preds, labels)
+        file.write(f'{score:.3f}, {prompt}' + '\n' + '\n')
+        print(f'{score:.3f}, {prompt}' + '\n' + '\n')
